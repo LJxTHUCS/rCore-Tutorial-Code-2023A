75d7c06f5a6feea3ad064e197f84dad94e4a2631
liujingx <liujingx21@mails.tsinghua.edu.cn>
2024-02-26 12:04:04 +0000

    feat: sys_get_time and sys_task_info

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,8 @@
 //! Process management syscalls
 use crate::{
-    config::MAX_SYSCALL_NUM,
-    task::{
-        change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
-    },
+    config::MAX_SYSCALL_NUM, mm::translated_ptr, task::{
+        change_program_brk, current_user_token, exit_current_and_run_next, get_start_time, get_syscall_times, suspend_current_and_run_next, TaskStatus
+    }, timer::{get_time_ms, get_time_us}
 };
 
 #[repr(C)]
@@ -41,17 +40,39 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let token = current_user_token();
+    let time = get_time_us();
+    unsafe {
+        let sec_ptr = translated_ptr(token, (&mut (*ts).sec) as *mut usize);
+        *sec_ptr = time / 1_000_000;
+        let usec_ptr = translated_ptr(token, (&mut (*ts).usec) as *mut usize);
+        *usec_ptr = time % 1_000_000;
+    }
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TaskInfo`] is splitted by two pages ?
-pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
-    -1
+    let token = current_user_token();
+    let status = TaskStatus::Running;
+    let syscall_times = get_syscall_times();
+    let time = get_time_ms() - get_start_time();
+    unsafe {
+        let status_ptr = translated_ptr(token, (&mut (*ti).status) as *mut TaskStatus);
+        *status_ptr = status;
+        for i in 0..MAX_SYSCALL_NUM {
+            let syscall_times_i_ptr = translated_ptr(token, (&mut (*ti).syscall_times[i]) as *mut u32);
+            *syscall_times_i_ptr = syscall_times[i];
+        }
+        let time_ptr = translated_ptr(token, (&mut (*ti).time) as *mut usize);
+        *time_ptr = time;
+    }
+    0
 }
 
 // YOUR JOB: Implement mmap.
